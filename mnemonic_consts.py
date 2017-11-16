ce5f336a2e2fdb8c14826bcd7f761219d9c0eeb1
Atif Mahadik <atif@phantom.us>
2017-11-15 18:38:14 -0800

    PAPP-3527 1st Cut of Mnemonic

---
+++ mnemonic_consts.py
@@ -0,0 +1,16 @@
+# --
+# File: mnemonic_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2017
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber Corporation.
+#
+# --
+
+MNEMONIC_BASE_URL = "https://api.mnemonic.no/pdns/v3"
+MNEMONIC_DEFAULT_RANGE = '0-100'
