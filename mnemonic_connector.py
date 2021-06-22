7cc55f5b82bc50ea02c7be8cf1e67dbb27145b3e
Ravindra Sojitra <rsojitra@splunk.com>
2021-06-22 10:52:15 +0530

    PAPP-17945: NRI checks.

--- mnemonic_connector.py
+++ mnemonic_connector.py
@@ -319,7 +319,7 @@ if __name__ == '__main__':
     if username and password:
         try:
             print("Accessing the Login page")
-            r = requests.get("https://127.0.0.1/login", verify=False)
+            r = requests.get("{}login".format(BaseConnector._get_phantom_base_url()), verify=False)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -329,10 +329,10 @@ if __name__ == '__main__':
 
             headers = dict()
             headers['Cookie'] = 'csrftoken=' + csrftoken
-            headers['Referer'] = 'https://127.0.0.1/login'
+            headers['Referer'] = "{}login".format(BaseConnector._get_phantom_base_url())
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post("https://127.0.0.1/login", verify=False, data=data, headers=headers)
+            r2 = requests.post("{}login".format(BaseConnector._get_phantom_base_url()), verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platfrom. Error: " + str(e))
